b536faf34d395361bb6418dea86e104dd300e8bc
Ricky Vila <rvila@chapman.edu>
2019-10-16 06:44:54 +0000

    3rd commit

--- main.cpp
+++ main.cpp
@@ -5,13 +5,24 @@
 int main(int argc, char **argv) {
 
   string fileName;
-  if(argc==2){
-    fileName = argv[1];
+  string userinput;
+  bool restart = true;
+  while(restart){
+    if(argc==2){
+      fileName = argv[1];
+    }
+
+    SyntaxChecker s;
+    s.readFile(fileName);
+    s.countDelims();
+
+    cout << "Would you like to exit the program? (y/n) \n";
+    cin >> userinput;
+    if(userinput == "no" || userinput == "n"){
+      break;
+    }
+    cout << "Enter another file name\n";
+    cin >> fileName;
   }
-
-  SyntaxChecker s;
-  s.readFile(fileName);
-  s.countDelims();
-
   return 0;
 }
